55c19b27259367751a15f2ded9d669b9e0501ff0
LostArrows27 <thelastofus2isnuts@gmail.com>
2025-07-25 21:59:17 +0700

    feat: ✨ configure eslint + husky + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,29 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { files: ['**/*.ts'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    languageOptions: {
+      globals: globals.node, parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+      'no-useless-catch': 0,
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'prefer-const': ['warn'],
+      'no-case-declarations': ['off']
+    },
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  prettierConfig
+
 ];
 
 export default eslintConfig;
