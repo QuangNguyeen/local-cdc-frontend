d58d4914110c65a6305d5f19d5da2ee9db4225ed
nad1011 <haidan10112003@gmail.com>
2025-07-26 12:52:10 +0700

    AAC-6 [REFACTOR]: Remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import { useTheme } from "next-themes"
 
 export default function HomePage() {
-    const { theme, setTheme } = useTheme()
-
     return (
-        <main className="flex min-h-screen items-center justify-center bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+        <main className="flex min-h-screen gap-2 items-center justify-center bg-background">
             <Button className="drak">Dark</Button>
             <Button className="light">Light</Button>
         </main>
